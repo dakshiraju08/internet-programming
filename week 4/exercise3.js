e86e1f1c1a456e6f990c2a6c2072dc5a1773b557
dakshiraju08 <100939087+dakshiraju08@users.noreply.github.com>
2022-03-23 16:48:02 +0000

    Changing element value when page changes

---
+++ week 4/exercise3.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    var element=document.getElementById("id1");
+    element.firstChild.nodeValue="New Heading";
+    var element2=document.getElementById("id2");
+    element2.firstChild.nodeValue="New content in paragraph";
+}
+
+document.addEventListener('DOMContentLoaded',myFunction);
+
